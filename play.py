6c6f4cb0d29636ce0f6cffe7e39c159659d08125
Neal Matta <nealmatt@umich.edu>
2020-02-07 18:55:36 -0500

    Why is this so much harder than I thought it'd be

---
+++ play.py
@@ -0,0 +1,16 @@
+# cv2 not working for some reason
+
+
+# import cv2
+
+
+# cap = cv2.VideoCapture("v2.mp4")
+# ret, frame = cap.read()
+# while(1):
+#    ret, frame = cap.read()
+#    cv2.imshow('frame',frame)
+#    if cv2.waitKey(1) & 0xFF == ord('q') or ret==False :
+#        cap.release()
+#        cv2.destroyAllWindows()
+#        break
+#    cv2.imshow('frame',frame)
